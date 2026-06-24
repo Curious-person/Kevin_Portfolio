eb9de5947ffbac4c92d5771f6d14189893d3db28
Kevin Abgao <kabgao@gmail.com>
2026-06-24 19:43:39 +0800

    feat: implement home page layout with sidebar and project image cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           description="Document Management System"
           imageSource="/images/nuspace_mockup.mp4" />
         <ImageCard />
-        <ImageCard imageSource={"https://images.unsplash.com/photo-1581345837712-414b9b6fb450?q=80&w=1076&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} />
+        <ImageCard />
         <ImageCard
           title="Project ATL - Antipolo TODA Map"
           description="Interactive Map with Real-time TODA Tracking"
